52db7e6b4805b12146c7adf86ff6fc69ae0053f0
Akash Mozumdar <akashmozumdar@gmail.com>
2019-06-29 15:28:12 +0530

    update documentation

--- ExampleExtension/Extension.cpp
+++ ExampleExtension/Extension.cpp
@@ -17,19 +17,21 @@ BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved
 //#define COPY_CLIPBOARD
 //#define EXTRA_NEWLINES
 
-/**
-	* Param sentence: sentence received by Textractor (UTF-16).
-	* Param sentenceInfo: contains miscellaneous info about the sentence (see README).
-	* Return value: whether the sentence was modified.
-	* Textractor will display the sentence after all extensions have had a chance to process and/or modify it.
-	* THIS FUNCTION MAY BE RUN SEVERAL TIMES CONCURRENTLY: PLEASE ENSURE THAT IT IS THREAD SAFE!
+/*
+	Param sentence: sentence received by Textractor (UTF-16). Can be modified, Textractor will receive this modification only if true is returned.
+	Param sentenceInfo: contains miscellaneous info about the sentence (see README).
+	Return value: whether the sentence was modified.
+	Textractor will display the sentence after all extensions have had a chance to process and/or modify it.
+	The sentence will be destroyed if it is empty or if you call Skip().
+	This function may be run concurrently with itself: please make sure it's thread safe.
+	It will not be run concurrently with DllMain.
 */
 bool ProcessSentence(std::wstring& sentence, SentenceInfo sentenceInfo)
 {
 	// Your code here...
 #ifdef COPY_CLIPBOARD
 	// This example extension automatically copies sentences from the hook currently selected by the user into the clipboard.
-	if (sentenceInfo["current select"] == 1)
+	if (sentenceInfo["current select"])
 	{
 		HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, (sentence.size() + 2) * sizeof(wchar_t));
 		memcpy(GlobalLock(hMem), sentence.c_str(), (sentence.size() + 2) * sizeof(wchar_t));
--- README.md
+++ README.md
@@ -12,6 +12,8 @@ After the sentence has been processed by all extensions, it will be displayed.
 ```"process id"```: process id that the sentence is coming from. 0 for console and clipboard.<br>
 ```"text number"```: number of the current text thread. Counts up one by one as text threads are created. 0 for console, 1 for clipboard.<br>
 ```"text name"```: pointer to start of a wchar array of the name of the current text thread.<br>
+```"void (*AddSentence)(void* this, int64_t number, const wchar_t* sentence)"```: pointer to function that adds a sentence to the text thread with the specified number.<br>
+```"this"```: context pointer used for aforementioned function.
 
 # Notes
 
